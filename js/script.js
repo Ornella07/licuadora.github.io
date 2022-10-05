9a4480ccd9dce294c06a37e3aa81788a6d8ec80a
Ornella Sereno Alloa <90015172+Ornella07@users.noreply.github.com>
2022-10-05 12:21:24 -0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -9,26 +9,15 @@ var licuadora = document.getElementById("blender");
 function controlarLicuadora() {
     if(estadoLicuadora == false){
         estadoLicuadora = true;
-        hacerBrrBrr();
+       
         licuadora.classList.add('active');
         
     }else{
         estadoLicuadora = false;
-        hacerBrrBrr();
+        
         licuadora.classList.remove('active');
         
         
     }
 }
 
-
-function hacerBrrBrr() {
-    if( sonidoLicuadora.paused ){
-        botonLicuadora.play();
-        sonidoLicuadora.play();
-      } else {
-        botonLicuadora.play();
-        sonidoLicuadora.pause();
-        sonidoLicuadora.currentTime = 0; /* Reinicia el audio al segundo 0 */
-      }
-    }
